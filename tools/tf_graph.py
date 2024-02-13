2935603b73d8f90b61298a505b9ebd341a56bcae
gogojjh <jiaojh1994@gmail.com>
2024-02-13 23:14:55 +0000

    add tf_tree visualization

--- tools/tf_graph.py
+++ tools/tf_graph.py
@@ -1,5 +1,4 @@
 import numpy as np
-import networkx as nx
 import matplotlib.pyplot as plt
 from collections import deque
 
@@ -23,12 +22,10 @@ class Edge:
 class TFGraph:
     def __init__(self):
       self.nodes = {}
-      self.G = nx.Graph()  # Initialize a new Graph object
     
     def add_node(self, frame_id):
       if frame_id not in self.nodes:
         self.nodes[frame_id] = Node(frame_id)
-        self.G.add_node(frame_id)  # Add node to the networkx graph
     
     def connect_nodes(self, from_node_name, to_node_name, tf_matrix):
       """
@@ -43,7 +40,6 @@ class TFGraph:
         from_node.edges.append(edge)
         edge = Edge(to_node, from_node, np.linalg.inv(tf_matrix))
         to_node.edges.append(edge)
-        self.G.add_edge(from_node_name, to_node_name, weight=1.0)
       else:
         print("One or both nodes do not exist.")
     
@@ -78,35 +74,79 @@ class TFGraph:
         return None
 
     def visualize_graph(self):
-      pos = nx.spring_layout(self.G)  # Positions for all nodes
-      nx.draw(self.G, pos, with_labels=True, node_color='skyblue', node_size=700, edge_color='k', linewidths=1, font_size=7, )
-      edge_labels = dict([((u, v,), d['weight']) for u, v, d in self.G.edges(data=True)])
-      nx.draw_networkx_edge_labels(self.G, pos, edge_labels=edge_labels)
+      from scipy.spatial.transform import Rotation as R
+      fig = plt.figure()
+      ax = fig.add_subplot(111, projection='3d')
+      
+      base_origin = np.array([0, 0, 0])
+      if 'body_imu' not in self.nodes:
+        print('The base frame_id does not exist.')
+        return
+
+      x_axis = np.array([1, 0, 0])
+      y_axis = np.array([0, 1, 0])
+      z_axis = np.array([0, 0, 1])
+      for frame_id, node in self.nodes.items():
+        if frame_id == 'body_imu':
+          # Frame
+          ax.quiver(base_origin[0], base_origin[1], base_origin[2], x_axis[0], x_axis[1], x_axis[2], length=0.1, normalize=True, color=[1, 0, 0], arrow_length_ratio=0.3)
+          ax.quiver(base_origin[0], base_origin[1], base_origin[2], y_axis[0], y_axis[1], y_axis[2], length=0.1, normalize=True, color=[0, 1, 0], arrow_length_ratio=0.3)
+          ax.quiver(base_origin[0], base_origin[1], base_origin[2], z_axis[0], z_axis[1], z_axis[2], length=0.1, normalize=True, color=[0, 0, 1], arrow_length_ratio=0.3)
+          ax.text(base_origin[0], base_origin[1], base_origin[2], frame_id, fontsize=10, color=[0, 0, 0])
+        if frame_id != 'body_imu':
+          tf_base_sensor = self.get_relative_transform('body_imu', frame_id)
+          if tf_base_sensor is not None:
+            origin = tf_base_sensor[:3, 3]
+            rotation = tf_base_sensor[:3, :3]
+            
+            # Link
+            ax.quiver(base_origin[0], base_origin[1], base_origin[2], origin[0], origin[1], origin[2], length=np.linalg.norm(origin), normalize=True, color=[0, 0, 0], arrow_length_ratio=0.05)
+            ax.text(origin[0], origin[1], origin[2], frame_id, fontsize=10, color=[0, 0, 0])
+            
+            new_x_axis = rotation @ x_axis
+            new_y_axis = rotation @ y_axis
+            new_z_axis = rotation @ z_axis
+            # Frame
+            ax.quiver(origin[0], origin[1], origin[2], new_x_axis[0], new_x_axis[1], new_x_axis[2], length=0.1, normalize=True, color=[1, 0, 0], arrow_length_ratio=0.3)
+            ax.quiver(origin[0], origin[1], origin[2], new_y_axis[0], new_y_axis[1], new_y_axis[2], length=0.1, normalize=True, color=[0, 1, 0], arrow_length_ratio=0.3)
+            ax.quiver(origin[0], origin[1], origin[2], new_z_axis[0], new_z_axis[1], new_z_axis[2], length=0.1, normalize=True, color=[0, 0, 1], arrow_length_ratio=0.3)
+
+      ax.set_xlim(-0.05, 0.35)
+      ax.set_ylim(-0.5, 0.5)
+      ax.set_zlim(-0.1, 0.1)
+      ax.set_xlabel('X')
+      ax.set_ylabel('Y')
+      ax.set_zlabel('Z')
+      ax.set_aspect('equal')
+      ax.view_init(elev=45, azim=190, roll=0)
       plt.show()
 
 def TEST():
   import random
   graph = TFGraph()
-  graph.add_node('A')
-  graph.add_node('B')
-  graph.add_node('C')
-  graph.add_node('D')
-  graph.add_node('E')
-  graph.add_node('F')
-  graph.add_node('G')
-  graph.add_node('H')
-  graph.connect_nodes('A', 'B', np.eye(4, 4))
-  graph.connect_nodes('A', 'C', np.eye(4, 4))
-  graph.connect_nodes('A', 'F', np.eye(4, 4))
-  graph.connect_nodes('B', 'D', np.eye(4, 4))
-  graph.connect_nodes('B', 'E', np.eye(4, 4))
-  graph.connect_nodes('F', 'G', np.eye(4, 4))
-  graph.connect_nodes('B', 'H', np.eye(4, 4))
-  graph.connect_nodes('A', 'H', np.eye(4, 4))
-  print(graph.get_relative_transform('A', 'D'))
-  print(graph.get_relative_transform('A', 'G'))
-  print(graph.get_relative_transform('E', 'G'))
-  print(graph.get_relative_transform('E', 'C'))  
+  graph.add_node('body_imu')
+  graph.add_node('ouster00')
+  # graph.add_node('C')
+  # graph.add_node('D')
+  # graph.add_node('E')
+  # graph.add_node('F')
+  # graph.add_node('G')
+  # graph.add_node('H')
+  # graph.connect_nodes('A', 'B', np.eye(4, 4))
+  # graph.connect_nodes('A', 'C', np.eye(4, 4))
+  # graph.connect_nodes('A', 'F', np.eye(4, 4))
+  # graph.connect_nodes('B', 'D', np.eye(4, 4))
+  # graph.connect_nodes('B', 'E', np.eye(4, 4))
+  # graph.connect_nodes('F', 'G', np.eye(4, 4))
+  # graph.connect_nodes('B', 'H', np.eye(4, 4))
+  # graph.connect_nodes('A', 'H', np.eye(4, 4))
+  # print(graph.get_relative_transform('A', 'D'))
+  # print(graph.get_relative_transform('A', 'G'))
+  # print(graph.get_relative_transform('E', 'G'))
+  # print(graph.get_relative_transform('E', 'C'))
+  tf = np.eye(4, 4)
+  tf[:3, 3] = np.array([0, 0, 0.2])
+  graph.connect_nodes('body_imu', 'ouster00', tf)
   graph.visualize_graph()
 
 if __name__ == "__main__":
