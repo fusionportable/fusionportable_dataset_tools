1432ac37c54f8478296c67f527d9ee3ce5da1547
gogojjh <jiaojh1994@gmail.com>
2024-02-18 20:46:54 +0000

    add feat: write alg_odometry to kitti data

--- cfg/algorithm/cfg_r3live.py
+++ cfg/algorithm/cfg_r3live.py
@@ -2,9 +2,9 @@
 
 algorithm_rostopic_msg_frameid_dict = {
   'ouster_points_undistorted': ['/r3live/cloud_nonregistered_raw', 'sensor_msgs/PointCloud2', 'r3live_lidar'],
-  'camera_render_pts': ['/r3live/render_pts', 'sensor_msgs/PointCloud2', 'r3live_world'],
+  'camera_render_pts': ['/r3live/render_pts', 'sensor_msgs/PointCloud2', 'r3live_camera'],
   'odometry': ['/r3live/aft_mapped_to_init', 'nav_msgs/Odometry', 'r3live_world'],
-  'camera_odometry': ['/r3live/camera_odom', 'sensor_msgs/Odometry', 'r3live_camera'],
+  'camera_odometry': ['/r3live/camera_odom', 'sensor_msgs/Odometry', 'r3live_world'],
   'path': ['/r3live/path', 'nav_msgs/Path', 'r3live_world'],
   'camera_path': ['/r3live/camera_path', 'nav_msgs/Path', 'r3live_world']
 }
