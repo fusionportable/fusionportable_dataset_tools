ff1e87280b3f47d1a49711b10e99a4a8aea30499
JokerJohn <2022087641@qq.com>
2024-06-14 08:42:43 +0800

    modify cloudmap evaluation

--- evaluation/map_evaluation/cloud_map_evaluation/src/cloud_map_eval_node.cpp
+++ evaluation/map_evaluation/cloud_map_evaluation/src/cloud_map_eval_node.cpp
@@ -16,13 +16,13 @@ int main(int argc, char **argv) {
     int method = 2;
     // max correspondence pairs distance for icp knn search correspondence
     // iteration(the search radius for kdtree)
-    //    double icp_max_distance = 1.0;  for large scale outdoor map
+    // double icp_max_distance = 1.0;  for large scale outdoor map
     double icp_max_distance = 0.5;
 
     // set evaluation accuracy level, do not recommend to change, eg. 20cm/10cm/5cm/2cm/1cm
     Vector5d accuracy_level = Vector5d::Zero();
+    // for outdoor map evaluation, we can set accuracy_level as  0.5, 0.3, 0.1, 0.05, 0.03;
     accuracy_level << 0.2, 0.1, 0.05, 0.02, 0.01;
-    //    accuracy_level << 0.5, 0.3, 0.1, 0.05, 0.03;
 
     // for map produced by LIO, we need a initial pose
     // we can use cloud compare to align your maps to get the initial pose
@@ -30,13 +30,18 @@ int main(int argc, char **argv) {
 
     // the path dir must end with '/'
     std::string est_path, gt_path, results_path, sequence_name;
+    // set your map folder
     std::string est_folder = "/home/xchu/my_git/Cloud_Map_Evaluation/cloud_map_evaluation/dataset/";
     sequence_name = "MCR_slow";
+    // the estimated map file path
     est_path = est_folder + sequence_name + "/";
+    // the ground truth map file path
     gt_path = est_folder + sequence_name + "/" + sequence_name + "_gt.pcd";
+    // the result will be saved in this folder
     results_path = est_folder + sequence_name + "/";
 
-    // if you evaluate mme
+    // if you evaluate mme (cost too much time, do not recommend to use it)
+    // but you do not have ground truth map, you can set evaluate_mme = true
     bool evaluate_mme = false;
     bool evaluate_gt_mme = false;
     Param my_param(est_path, gt_path, results_path, initial_matrix, sequence_name,
