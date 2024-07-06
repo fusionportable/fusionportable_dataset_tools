e990b1103ca33ab4fcb78928ce60a54f2987b2da
gogojjh <jiaojh1994@gmail.com>
2024-07-06 14:10:16 +0100

    add traj_calibrate_odom

--- tools/encoder_calib/draw_odom.m
+++ tools/encoder_calib/draw_odom.m
@@ -1,7 +1,7 @@
 clc; clear; close all;
 color_list = acquire_color_list();
 
-%%
+%% Set filename and load dasta
 filename = 'example_data/ugv_parking00/RTK.txt';
 filename2 = 'example_data/ugv_parking00/CAD.txt';
 filename3 = 'example_data/ugv_parking00/Calibrated.txt';
@@ -9,42 +9,26 @@ table_data = readtable(filename);
 table_data2 = readtable(filename2);
 table_data3 = readtable(filename3);
 
-%%
 data = parse_table_data(table_data);
 data2 = parse_table_data(table_data2);
 data3 = parse_table_data(table_data3);
 
+%% Draw the trajectory
 figure(1); 
 hold on;
 plot3(data.x, data.y, data.z, 'Color', color_list(1, 1:3), 'LineWidth', 2, 'LineStyle', '-');
-% plot3(data.x, data.y, data.z, '*', 'Color', color_list(1, 1:3));
-% plot3(data.x(1), data.y(1), data.z(1), 'o', 'Color', color_list(2, 1:3), ...
-%   'MarkerSize', 20, 'MarkerFaceColor', color_list(3, 1:3));
-% plot3(data.x(end), data.y(end), data.z(end), 'd', 'Color', color_list(3, 1:3), ...
-%   'MarkerSize', 20, 'MarkerFaceColor', color_list(4, 1:3));
-
 plot3(data2.x, data2.y, data2.z, 'Color', color_list(2, 1:3), 'LineWidth', 2, 'LineStyle', '-');
-
 plot3(data3.x, data3.y, data3.z, 'Color', color_list(4, 1:3), 'LineWidth', 2, 'LineStyle', '--');
-% plot3(data.x, data.y, data.z, '*', 'Color', color_list(1, 1:3));
-% plot3(data2.x(1), data2.y(1), data2.z(1), 'o', 'Color', color_list(2, 1:3), ...
-%   'MarkerSize', 20, 'MarkerFaceColor', color_list(3, 1:3));
-% plot3(data2.x(end), data2.y(end), data2.z(end), 'd', 'Color', color_list(3, 1:3), ...
-%   'MarkerSize', 20, 'MarkerFaceColor', color_list(4, 1:3));
+plot3(0, 0, 0, 'd', 'Color', color_list(5, 1:3), 'MarkerSize', 20, 'MarkerFaceColor', color_list(5, 1:3));
 sprintf('t.secs>=%.9f and t.secs<=%.9f', data.time(1), data.time(end))
 
-legend({'Motion of 3DM-GQ7', 'Motion of Encoders (Before Calib.)', 'Motion of Encoders (After Calib.)'}, 'Location', 'northeast', 'FontSize', 25,'fontname','Times New Roman');
-% legend({'Method 2'}, 'Location', 'northwest', 'FontSize', 20);
+legend({'Motion of INS', 'Motion of Encoders (Before Calib.)', 'Motion of Encoders (After Calib.)'}, 'Location', 'northeast', 'FontSize', 25,'fontname','Times New Roman');
 grid on; ax = gca; ax.GridLineStyle = '--'; ax.GridAlpha = 0.3; box on;
 set(gca, 'FontName', 'Times', 'FontSize', 25, 'LineWidth', 2);
 xlabel("X [m]"); ylabel("Y [m]"); zlabel("Z [m]");
 
-hold off;
-axis on; 
-axis equal;
-grid on;
-xlim([-17, 15]);
-ylim([-1, 28]);
+hold off; axis on; axis equal; grid on;
+xlim([-2, 48]); ylim([-12, 33]);
 
 %%
 function data = parse_table_data(table_data)
